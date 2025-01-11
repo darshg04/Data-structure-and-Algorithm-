497567fee8267d8c3a9575b8cf6dcfaf6d0ee288
Darshan Gohel <darshangohel0008@gmail.com>
2025-01-11 08:27:51 -0800

    leastNum

--- Function/defination.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class defination {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(n);
-    }
-}
